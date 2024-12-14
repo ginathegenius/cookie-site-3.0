d4629d96f85196869a2d4739cc2891714d45b9f2
Gina Bueno <ginrubu@gmail.com>
2024-12-14 17:43:12 -0500

    update filter styling

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Accordion from './Accordion'; // Import the Accordion component
+import Accordion from 'react-bootstrap/Accordion';
+// import Accordion from './Accordion'; // Import the Accordion component
 import { Container, Row, Col } from 'react-bootstrap';
 
 const FilterComponent = ({ yearFilters, handleYearChange, allergyFilters, handleAllergyChange, flavorFilters, handleFlavorChange }) => {
@@ -9,28 +10,37 @@ const FilterComponent = ({ yearFilters, handleYearChange, allergyFilters, handle
         <div>
             <Container fluid>
                 <Row>
-                    <Col>
-            <Accordion title="Year:">
-                {Object.keys(yearFilters).map((year) => (
-                    <div key={year} className="form-check">
-                    <label className="form-check-label" htmlFor={`flexCheckDefault${year}`}>
-                    <input
-                        className="form-check-input"
-                        type="checkbox"
-                        name={year}
-                        id={`flexCheckDefault${year}`}
-                        checked={yearFilters[year]}
-                        onChange={handleYearChange}
-                    />
-                        {year}
-                    </label>
-                </div>
-                ))}
-
-            </Accordion>
+                    <Col xs={12} sm={12} md={4}>
+                    <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">            
+                    <Accordion.Header>Year</Accordion.Header>
+                    <Accordion.Body>
+                        {Object.keys(yearFilters).map((year) => (
+                            <div key={year} className="form-check">
+                            <label className="form-check-label" htmlFor={`flexCheckDefault${year}`}>
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                name={year}
+                                id={`flexCheckDefault${year}`}
+                                checked={yearFilters[year]}
+                                onChange={handleYearChange}
+                            />
+                                {year}
+                            </label>
+                        </div>
+                        ))}
+                    </Accordion.Body>
+                </Accordion.Item>
+                </Accordion>
             </Col>
-<Col>
-            <Accordion title="Exclude Allergens:">
+            <Col xs={12} sm={12} md={4}>
+            <Accordion>
+                <Accordion.Item eventKey="0">     
+                    <Accordion.Header>Exclude Allergies</Accordion.Header>
+                        <Accordion.Body>
+
+
                 {Object.keys(allergyFilters).map((allergy) => (
                     <div key={allergy} className="form-check">
                         <label className="form-check-label" htmlFor={`flexCheck${allergy}`}>
@@ -46,10 +56,15 @@ const FilterComponent = ({ yearFilters, handleYearChange, allergyFilters, handle
                         </label>
                     </div>
                 ))}
+                        </Accordion.Body>
+                </Accordion.Item>
             </Accordion>
             </Col>
-            <Col>
-            <Accordion title="Include Flavors:">
+            <Col xs={12} sm={12} md={4}>
+            <Accordion>
+                <Accordion.Item eventKey="0">     
+                    <Accordion.Header>Include Flavors</Accordion.Header>
+                        <Accordion.Body>
                 {Object.keys(flavorFilters).map((flavor) => (
                     <div key={flavor} className="form-check">
                         <label className="form-check-label" htmlFor={`flexCheck${flavor}`}>
@@ -66,6 +81,9 @@ const FilterComponent = ({ yearFilters, handleYearChange, allergyFilters, handle
                         </label>
                     </div>
                 ))}
+                
+               </Accordion.Body>
+               </Accordion.Item>
             </Accordion>
             </Col>
             </Row>
